333ec63a5df3c2a64590afc1a529ae82100af5cb
jopoepl <joel_joseph93@outlook.com>
2022-05-12 08:05:10 +0530

    Assgn Level 3 JS

--- block-BJaabg/code/index.js
+++ block-BJaabg/code/index.js
@@ -13,6 +13,46 @@ const ACCESSORY_PRICE = 9.99;
 
 var bank_balance = 303.91;
 var amount = 0;
+
+
+while (303.91 > amount) {
+    amount += PHONE_PRICE; // Phone price increases as long as amount is less than 303.91
+    if (amount < SPENDING_THRESHOLD) { //Decides on whether to add accessory price
+        amount += ACCESSORY_PRICE;
+        bank_balance -= (PHONE_PRICE + ACCESSORY_PRICE);
+    } else {
+        bank_balance -= PHONE_PRICE;
+    }
+    
+}
+
+alert(`$` + amount * (1 + TAX_RATE));
+
 // your code goes here
 
+const SPENDING_THRESHOLD = 200;
+const TAX_RATE = 0.08;
+const PHONE_PRICE = 99.99;
+const ACCESSORY_PRICE = 9.99;
+
+var bank_balance = 303.91;
+var amount = 0;
+var finalamount = 0;
+var i = 0;
+
+while (amount < 303.91) {
+    i++
+    amount = (PHONE_PRICE) * i;
+    if (amount < SPENDING_THRESHOLD ) {
+        amount += ACCESSORY_PRICE
+        bank_balance -= (PHONE_PRICE + ACCESSORY_PRICE);
+    } else {
+        bank_balance -= PHONE_PRICE;
+    }
+    
+    console.log(amount, bank_balance)
+}
+
+
 // ⛑ Answer of the above will `$334.76`.
+
